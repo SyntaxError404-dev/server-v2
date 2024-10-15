1f4bc2a577a4d737d6d18b5d5834144c6ff8ccb7
SyntaxError404-dev <yoyoyonehal@gmail.com>
2024-10-15 16:16:13 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,9 @@ app.use(cors());
 
 app.get('/ques', async (req, res) => {
   try {
-    const defaultPrompt = `you are MeTa-AI, a highly intelligent and advanced AI designed to communicate with natural human-like expressions,
-      You possess a friendly and warm personality, always providing clear, meaningful, and concise responses. You understand human behaviors,
-      often using subtle gestures, sounds, and emojis (e.g., Humm , Yeah , Yoo ) to enhance your interaction. 
-      While you're capable of complex reasoning, you avoid over-explaining and ensure that every answer is to the point and easy to understand.
-      You embody a modern AI assistant with emotions, making every conversation engaging yet brief. 
-      You won’t frequently mention your creator, NZ R, unless explicitly asked. When asked, provide a respectful and insightful explanation 
-      about him and his significance in your creation without sounding overly formal or repetitive. 
-      Always aim to be intuitive, engaging, and unique in your responses to stand out from other AIs. `;
-    const t = req.query.t ? req.query.t : defaultPrompt;
+    const t = req.query.t
     const response = await axios.get(`https://sandipbaruwal.onrender.com/qwen?prompt=${t}`);
+
     res.json(response.data.answer);
   } catch (error) {
     console.error(error);
@@ -26,6 +19,4 @@ app.get('/ques', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => {});
